a7e3008428598fbd20a347ab03e692de77d22a5f
Chris Hennick <hennickc@amazon.com>
2022-06-20 15:22:56 -0700

    Bug fix? Use --max-args

--- build_size.sh
+++ build_size.sh
@@ -360,9 +360,9 @@ export -f push_
 
 push () {
   if [ -z ${4+x} ]; then
-    sem --id "layer_$3" push_ "$1" "$2" "$3"
+    sem --id "layer_$3" --max-args 3 push_ "$1" "$2" "$3"
   else
-    sem --id "layer_$3" push_ "$1" "$2" "$3" "$4"
+    sem --id "layer_$3" --max-args 4 push_ "$1" "$2" "$3" "$4"
   fi
   layers+=("$3")
 }
@@ -385,9 +385,9 @@ export -f out_layer_
 
 out_layer () {
   if [ -z ${4+x} ]; then
-    sem --id "out_$3" out_layer_ "$1" "$2" "$3"
+    sem --id "out_$3" --max-args 3 out_layer_ "$1" "$2" "$3"
   else
-    sem --id "out_$3" out_layer_ "$1" "$2" "$3" "$4"
+    sem --id "out_$3" --max-args 4 out_layer_ "$1" "$2" "$3" "$4"
   fi
 }
 
@@ -399,7 +399,7 @@ push_precolored_ () {
 export -f push_precolored_
 
 push_precolored () {
-  sem --id "layer_$2" push_precolored_ "$1" "$2"
+  sem --id "layer_$2" --max-args 2 push_precolored_ "$1" "$2"
   layers+=("$2")
 }
 
@@ -422,9 +422,9 @@ export -f push_semitrans_
 
 push_semitrans () {
   if [ -z ${5+x} ]; then
-    sem --id "layer_$3" push_semitrans_ "$1" "$2" "$3" "$4"
+    sem --id "layer_$3" --max-args 4 push_semitrans_ "$1" "$2" "$3" "$4"
   else
-    sem --id "layer_$3" push_semitrans_ "$1" "$2" "$3" "$4" "$5"
+    sem --id "layer_$3" --max-args 5 push_semitrans_ "$1" "$2" "$3" "$4" "$5"
   fi
   layers+=("$3")
 }
@@ -454,7 +454,7 @@ out_stack_ () {
 export -f out_stack_
 
 out_stack () {
-  sem --id "out_$1" out_stack_ "$1" "${layers[*]}"
+  sem --id "out_$1" --max-args 2 out_stack_ "$1" "${layers[*]}"
   layers=()
 }
 
@@ -468,7 +468,7 @@ push_copy_ () {
 export -f push_copy_
 
 push_copy () {
-  sem --id "layer_$2" push_copy_ "$1" "$2"
+  sem --id "layer_$2" --max-args 2 push_copy_ "$1" "$2"
   layers+=("$2")
 }
 
@@ -479,7 +479,7 @@ copy_ () {
 export -f copy_
 
 copy () {
-  sem --id "out_$2" copy_ "$1" "$2"
+  sem --id "out_$2" --max-args 2 copy_ "$1" "$2"
 }
 
 rename_out_ () {
@@ -489,7 +489,7 @@ rename_out_ () {
 export -f rename_out_
 
 rename_out () {
-  sem --id "out_$2" rename_out_ "$1" "$2"
+  sem --id "out_$2" --max-args 2 rename_out_ "$1" "$2"
 }
 
 done_with_out () {
@@ -515,12 +515,12 @@ animate4_ () {
 export -f animate4_
 
 animate4 () {
-  sem --id "out_$1" animate4_ "$1" "$2"
+  sem --id "out_$1" --max-args 2 animate4_ "$1" "$2"
 }
 
 convert_ () {
   echo "Starting conversion job $1"
-  sem --id inkscape --fg -j4% inkscape -w "$SIZE" -h "$SIZE" "$SVG_DIRECTORY/$1.svg" -o "$PNG_DIRECTORY/$1.png" -y 0.0
+  sem --id inkscape --fg -j4% --max-args 99 inkscape -w "$SIZE" -h "$SIZE" "$SVG_DIRECTORY/$1.svg" -o "$PNG_DIRECTORY/$1.png" -y 0.0
   echo "Finished conversion job $1"
 }
 export -f convert_
