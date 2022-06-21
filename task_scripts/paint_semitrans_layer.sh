c062a634c1987659e5a3ca009011d76bf7aa180c
Chris Hennick <hennickc@amazon.com>
2022-06-20 18:26:52 -0700

    Bug fix: remove # signs from color constants

--- task_scripts/paint_semitrans_layer.sh
+++ task_scripts/paint_semitrans_layer.sh
@@ -3,12 +3,12 @@ echo "push_semitrans_ arguments: input $1, fill $2, output $3; full args: $*"
 ./task_scripts/join_job.sh "convert_$1"
 if [ -z ${5+x} ]; then
   magick "$PNG_DIRECTORY/$1.png" \
-            -fill "$2" -colorize 100% \
+            -fill "#$2" -colorize 100% \
             -alpha set -background none -channel A -evaluate multiply "$4" +channel "$TMPDIR/$3.png"
 else
   magick "$PNG_DIRECTORY/$1.png" \
-                -fill "$2" -colorize 100% \
-                -background "$4" -alpha remove \
+                -fill "#$2" -colorize 100% \
+                -background "#$4" -alpha remove \
                 -alpha set -background none -channel A -evaluate multiply "$5" +channel "$TMPDIR/$3.png"
 fi
 echo "Wrote semitransparent layer $3"
