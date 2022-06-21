4168ca8148a2c9806744142f132e16d74f3a33c0
Chris Hennick <hennickc@amazon.com>
2022-06-20 19:38:20 -0700

    Bug fix?

--- task_scripts/convert.sh
+++ task_scripts/convert.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "STARTING convert_$1"
-parallel -m --id memory --fg -j"$MAX_TASKS" inkscape -w "$SIZE" -h "$SIZE" "$SVG_DIRECTORY/$1.svg" -o "$PNG_DIRECTORY/$1.png" -y 0.0
+parallel -m --id memory --fg --jobs "$MAX_TASKS" inkscape -w "$SIZE" -h "$SIZE" "$SVG_DIRECTORY/$1.svg" -o "$PNG_DIRECTORY/$1.png" -y 0.0
 echo "FINISHED convert_$1"
--- task_scripts/paint_semitrans_layer.sh
+++ task_scripts/paint_semitrans_layer.sh
@@ -2,11 +2,11 @@
 ./task_scripts/join_job.sh "convert_$1"
 echo "STARTING layer_$3"
 if [ -z ${5+x} ]; then
-  parallel -m --id memory --fg -j"$MAX_TASKS" magick "$PNG_DIRECTORY/$1.png" \
+  parallel -m --id memory --fg --jobs "$MAX_TASKS" magick "$PNG_DIRECTORY/$1.png" \
             -fill "#$2" -colorize 100% \
             -alpha set -background none -channel A -evaluate multiply "$4" +channel "$TMPDIR/$3.png"
 else
-  parallel -m --id memory --fg -j"$MAX_TASKS" magick "$PNG_DIRECTORY/$1.png" \
+  parallel -m --id memory --fg --jobs "$MAX_TASKS" magick "$PNG_DIRECTORY/$1.png" \
                 -fill "#$2" -colorize 100% \
                 -background "#$4" -alpha remove \
                 -alpha set -background none -channel A -evaluate multiply "$5" +channel "$TMPDIR/$3.png"
