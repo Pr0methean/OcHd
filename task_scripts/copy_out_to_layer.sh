16debbb5080ed2fcdb70453bd8ff73fb075cc0b0
Chris Hennick <hennickc@amazon.com>
2022-06-20 19:00:58 -0700

    Remove stray logging statements no longer needed

--- task_scripts/copy_out_to_layer.sh
+++ task_scripts/copy_out_to_layer.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-echo "push_copy_ arguments: old file $1, new file $2; full args: $*"
 ./task_scripts/join_job.sh "out_$1"
 echo "STARTING layer_$2"
 ln -T "$OUTDIR/$1.png" "$TMPDIR/$2.png"
--- task_scripts/render_stack.sh
+++ task_scripts/render_stack.sh
@@ -10,10 +10,8 @@ done
 echo "STARTING out_$1"
 OUTFILE="${OUTDIR}/$1.png"
 if [ ${#my_layers[@]} -eq 1 ]; then
-  echo "Copying ${layer_files[0]} to output $1"
   ln -T "${layer_files[0]}" "${OUTFILE}"
 else
-  echo "Building output $1 from layers: ${layer_files[*]}"
   magick "${layer_files[@]}"  -colorspace sRGB -background none -layers flatten -set colorspace RGBA "${OUTFILE}"
 fi
 echo "FINISHED out_$1"
