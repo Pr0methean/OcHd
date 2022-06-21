fc7cd323e1ca42294b4098b3f8142790d8ee0fb6
Chris Hennick <hennickc@amazon.com>
2022-06-20 19:49:01 -0700

    Test whether line break affects error message

--- task_scripts/paint_layer.sh
+++ task_scripts/paint_layer.sh
@@ -4,8 +4,6 @@ echo "STARTING layer_$3"
 if [ -z ${4+x} ]; then
   parallel -m --id memory --fg --jobs "$MAX_TASKS" magick "$PNG_DIRECTORY/$1.png" -fill "#$2" -colorize 100% "$TMPDIR/$3.png"
 else
-  parallel -m --id memory --fg --jobs "$MAX_TASKS" magick "$PNG_DIRECTORY/$1.png" \
-                -fill "#$2" -colorize 100% \
-                -background "#$4" -alpha remove -alpha off "$TMPDIR/$3.png"
+  parallel -m --id memory --fg --jobs "$MAX_TASKS" magick "$PNG_DIRECTORY/$1.png" -fill "#$2" -colorize 100% -background "#$4" -alpha remove -alpha off "$TMPDIR/$3.png"
 fi
 echo "FINISHED layer_$3"
